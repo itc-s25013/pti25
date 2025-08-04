ef6a793bf6e9a9ebab779b3e321737db8e871b5a
itc-s25013 <s25013@std.it-college.ac.jp>
2025-08-04 16:28:36 +0900

    バージョンアップ

--- omikuji.py
+++ omikuji.py
@@ -1,3 +1,8 @@
 import random
-kuji = ["大吉", "中吉", "小吉", "吉", "末吉", "凶", "大凶"]
-print(random.choice(kuji))
+def main():
+    kuji = ["大吉", "中吉", "小吉", "吉", "末吉", "凶", "大凶"]
+    result = random.choice(kuji)
+    print(result)
+
+if __name__ == "__main__":
+    main()
